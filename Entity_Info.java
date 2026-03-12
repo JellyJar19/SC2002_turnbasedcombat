9bf7534440ac5a60b58456fdd8af6a48ec409f54
Jarrett Lee <jarrettleehong@gmail.com>
2026-03-12 15:00:56 +0800

    developing one part of the thing
    I tried messing with the character creation and I think its a pretty basic thing as it stands now, just need to figure out what exactly our implementation of the levels would be

---
+++ Entity_Info.java
@@ -0,0 +1,68 @@
+public class Entity{
+    private int HP;
+    private int Atk;
+    private int Def;
+    private int Spd;
+    public Entity(int hitpoint, int attack, int defense, int Speed){
+        HP = hitpoint;
+        Atk = attack;
+        Def = defense;
+        Spd = Speed;
+    }
+    public void basicAttack(Entity target){
+        target.setHP(target.getHP() - this.Atk);
+    }
+    public void setHP(int hitpoint){
+        HP = hitpoint;
+    }
+    public void setAtk(int attack){
+        Atk = attack;
+    }
+    public void setDef(int defense){
+        Def = defense;
+    }
+    public void setSpd(int Speed){
+        Spd = Speed;
+    }
+    public int getHP(){
+        return this.HP;
+    }
+    public int getAttack(){
+        return this.Atk;
+    }
+    public int getDef(){
+        return this.Def;
+    }
+    public int getSpd(){
+        return this.Spd;
+    }
+}
+
+public class Item(){
+}
+
+public class Warrior extends Entity{
+    Item[] inven = new Item();
+    public Warrior(){
+        super();
+        this.inven = ;
+    }
+}
+
+public class Wizard extends Entity{
+    public Wizard(){
+        super();
+    }
+}
+
+public class Wolf extends Entity{
+    public Wolf(){
+        super();
+    }
+}
+
+public class Goblin extends Entity{
+    public Goblin(){
+        super();
+    }
+}
