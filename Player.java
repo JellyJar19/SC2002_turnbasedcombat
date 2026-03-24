c87fe83e85b9697a73c32943ffd5be8e88fc73ba
Kayennne <116442544+Kayennne@users.noreply.github.com>
2026-03-24 22:03:56 +0800

    update player class
    added methods + comments

--- Player.java
+++ Player.java
@@ -1,19 +1,31 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public abstract class Player extends Entity{
-    public Player(int hitpoint, int attack, int defense, int Speed) {
-        super(hitpoint, attack, defense, Speed);
-    }
-    public void Defend() {
+public abstract class Player extends Combatant{
+    protected List<Item> inventory;
 
+    public Player(String name, int hp, int attack, int defense, int speed) {
+        super(name, hp, attack, defense, speed); //take from combatant
+        this.inventory = new ArrayList<>();
     }
-    public void Special() {
+    
+    public List<Action> getAvailableAction(){
+        List<Action> action = new ArrayList<>();
+        action.add(new BasicAttackAction());
+        action.add(new DefendAction());
 
+        for (Item item:inventory){
+            action.add(new UseItemAction(item));
+        }
+        return action; // avail action list is attack + defend + items avail
     }
-    public void UseItem() {
 
+    public List<Item> getInventory(){
+        return inventory;
     }
-    public abstract String getSpecialDesc();
+
+    public void addItem(Item item){
+        inventory.add(item); // for player to add chosen item
+    }
 
 }
