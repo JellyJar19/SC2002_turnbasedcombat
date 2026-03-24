c1790153ffddd55ba06380192c0681d487ac0e65
Kayennne <116442544+Kayennne@users.noreply.github.com>
2026-03-24 23:24:07 +0800

    Update Warrior.java

--- Warrior.java
+++ Warrior.java
@@ -1,13 +1,15 @@
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.swing.Action;
+
 public class Warrior extends Player {
     public Warrior(String name){
         super(name, 260, 40, 20, 30);
     }
 
     public List<Action> getAvailableActions() {
-        List<Action> action = super.getAvailableAction();
+        List<Action> action = super.getAvailableAction(); // waiting for the class to be made
         action.add(new ShieldBashAction()); // add warrior special skill
         return action;
     }
