c5218e080949d99cf5d588f274f35ea80d3e512c
Ignatius Justin Low <143064742+justinnljw@users.noreply.github.com>
2026-03-24 14:43:10 +0800

    Update and rename Entity.java to Combatant.java

---
+++ Combatant.java
@@ -0,0 +1,132 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class Combatant {
+
+    protected String name;
+    protected int hp;
+    protected int maxHp;
+    protected int attack;
+    protected int defense;
+    protected int speed;
+    protected List<StatusEffect> statusEffects;
+    protected int skillCooldown;
+
+    // constructor
+    public Combatant(String name, int hp, int attack, int defense, int speed) {
+        this.name = name;
+        this.hp = hp;
+        this.maxHp = hp;
+        this.attack = attack;
+        this.defense = defense;
+        this.speed = speed;
+        this.statusEffects = new ArrayList<StatusEffect>();
+        this.skillCooldown = 0;
+    }
+
+    public void takeDamage(int amount) {
+        if (this.hp - amount < 0) {
+            this.hp = 0;
+        } 
+        else {
+            this.hp = this.hp - amount;
+        }
+    }
+    public void heal(int amount) {
+        if (this.hp + amount > this.maxHp) {
+            this.hp = this.maxHp;
+        } 
+        else {
+            this.hp = this.hp + amount;
+        }
+    }
+
+    // this one need see how keane does it
+    public void addStatusEffect(StatusEffect effect) {
+        statusEffects.add(effect);
+        effect.onApply(this);
+    }
+
+    // this one need see how keane does it
+    public void applyStatusEffects() {
+        for (StatusEffect effect : statusEffects) {
+            effect.onTurnStart(this);
+        }
+    }
+
+    // this one need see how keane does it
+    public void removeExpiredEffects() {
+        for (StatusEffect effect : statusEffects) {
+            effect.onTurnEnd(this);
+            effect.decreaseDuration();
+        }
+        statusEffects.removeIf(StatusEffect::isExpired);
+    }
+
+    public boolean isAlive() {
+        return this.hp > 0;
+    }
+
+    // this one need see how keane does it
+    public boolean canAct() {
+        for (StatusEffect effect : statusEffects) {
+            if (effect instanceof StunEffect) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void reduceCooldown() {
+        if (skillCooldown > 0) {
+            skillCooldown--;
+        }
+    }
+
+    // getters and setters
+    public abstract List<Action> getAvailableActions();
+
+    public String getName() { 
+        return name; 
+    }
+
+    public int getHp() { 
+        return hp; 
+    }
+
+    public int getMaxHp() { 
+        return maxHp; 
+    }
+
+    public int getAttack() { 
+        return attack; 
+    }
+
+    public int getDefense() { 
+        return defense; 
+    }
+
+    public int getSpeed() { 
+        return speed; 
+    }
+
+    public int getSkillCooldown() { 
+        return skillCooldown; 
+    }
+
+    public List<StatusEffect> getStatusEffects() { 
+        return statusEffects; 
+    }
+
+    public void setAttack(int attack) { 
+        this.attack = attack; 
+    }
+
+    public void setDefense(int defense) { 
+        this.defense = defense; 
+    }
+
+    public void setSkillCooldown(int cd) { 
+        this.skillCooldown = cd; 
+    }
+}
