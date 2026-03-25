a81959cdec32d5f1b6dcc8ec6073d468914d9450
Jarrett Lee <jarrettleehong@gmail.com>
2026-03-25 20:08:30 +0800

    Add files via upload

---
+++ UseItemAction.java
@@ -0,0 +1,15 @@
+public class UseItemAction implements Action{
+    private Item item;
+
+    UseItemAction(Item item){
+        this.item = item;
+    }
+    
+    @Override
+    public void execute(Combatant activechar, Battle battle, Combatant target){
+        item.Use(activechar, battle, target);
+    }
+    
+    public String getName(){return item.getName();}
+    
+}
