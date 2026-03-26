f70cd872900c10e61e44b9db1c3a43bd12f60af2
lollipop690 <kpkj.crazy@gmail.com>
2026-03-26 21:54:25 +0800

    minor touchups

--- StatusEffects.java
+++ StatusEffects.java
@@ -1,18 +1,15 @@
 public enum StatusEffects {
     SmokeBombEffect(){
         public void onApply(Combatant target){
-        //apply action onto target
         this.active=true;
         this.duration = 2;
     }
 
-    public void startTurn(Combatant target){
-        //actions to take during start of turn
+    public void startTurn(Combatant target){ 
         this.duration--;
     }
 
     public void endTurn(Combatant target){
-        //actions to take during end of turn
         if (this.duration==0){
             this.active=false;
         }
@@ -25,13 +22,11 @@ public enum StatusEffects {
     }
 
     public void startTurn(Combatant target){
-        //actions to take during start of turn
         this.duration--;
 
     }
 
     public void endTurn(Combatant target){
-        //actions to take during end of turn
         if (this.duration==0){
             this.active=false;
         }
@@ -45,13 +40,11 @@ public enum StatusEffects {
     }
 
     public void startTurn(Combatant target){
-        //actions to take during start of turn
         this.duration--;
 
     }
 
     public void endTurn(Combatant target){
-        //actions to take during end of turn
         if (this.duration==0){
             target.setStun(false);
             this.active=false;
@@ -66,11 +59,11 @@ public enum StatusEffects {
         this.active=false; //only become active when onApply is called
     }
 
-    abstract public void onApply(Combatant target);
+    abstract public void onApply(Combatant target); //to be called when action is acted on Characters
 
-    abstract public void startTurn(Combatant target);
+    abstract public void startTurn(Combatant target); //to be called at the start of each turn (if action is applied)
 
-    abstract public void endTurn(Combatant target);
+    abstract public void endTurn(Combatant target); //to be called at the end of each turn
 
     public boolean isActive(){
         return active;
