c404a83c1499a7832b5f3bc7a20e0d3e6671dd9b
Jarrett Lee <jarrettleehong@gmail.com>
2026-03-30 21:51:50 +0800

    Change target defense method from getDef to getDefense
    Aligning actions to combatant

--- BasicAttackAction.java
+++ BasicAttackAction.java
@@ -1,7 +1,7 @@
 public class BasicAttackAction implements Action {
     public void execute(Combatant activechar, Battle battle, Combatant target){
         int atk = activechar.getAttack();
-        int def = target.getDef();
+        int def = target.getDefense();
         int dmg = Math.Max(0,atk - def);
         target.takeDamage(dmg);
         // debating if i need to end turn there. 
