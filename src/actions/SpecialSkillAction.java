ee0413cd815b495b677f615e7468d232c815df15
lollipop690 <166970001+lollipop690@users.noreply.github.com>
2026-04-10 17:37:26 +0800

    some changes

--- src/actions/SpecialSkillAction.java
+++ src/actions/SpecialSkillAction.java
@@ -1,10 +1,12 @@
 package actions;
 import items.*;
 import entities.*;
+import battleEngine.*;
 public abstract class SpecialSkillAction implements Actions{
-    public void execute(Combatant activechar, Battle battle, Combatant target){};
+
+    public  void execute(Combatant activechar, Battle battle, Combatant target){};
     public boolean isOffCooldown(Combatant activechar){
-        if (activechar.skillcooldown > 0){
+        if (activechar.skillCooldown > 0){
             return false;
         }
         else return true;
