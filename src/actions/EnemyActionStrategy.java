096c195db0cb1bdb739a30179930977c034a2027
lollipop690 <166970001+lollipop690@users.noreply.github.com>
2026-04-10 19:48:09 +0800

    delete

--- src/actions/EnemyActionStrategy.java
+++
@@ -1,6 +0,0 @@
-public abstract class EnemyActionStrategy {
-    public Actions chooseAction(Enemy enemy, Battle currenBattle) {
-        System.out.print("Not implemented yet");
-        return(null);
-    };
-}
