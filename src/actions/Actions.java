1e03c75089a6769debca62fe72d917136e95799c
Jarrett Lee <jarrettleehong@gmail.com>
2026-04-10 19:47:04 +0800

    Delete src/actions directory

--- src/actions/Actions.java
+++
@@ -1,8 +0,0 @@
-package actions;
-import entities.*;
-import battleEngine.*;
-
-interface Actions{
-    public void execute(Combatant activechar, Battle battle, Combatant target);
-    public String getName();
-}
