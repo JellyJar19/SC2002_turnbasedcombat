d224565c6d19d1a0296221f7da1130e25fd0639d
Jarrett Lee <jarrettleehong@gmail.com>
2026-04-11 14:55:11 +0800

    Updated to work with StatusEffect

--- src/action/ArcaneBlast.java
+++ src/action/ArcaneBlast.java
@@ -7,19 +7,17 @@ public class ArcaneBlast extends BasicAttack implements Action{
     public <T> boolean execute(T activechar, T target, Battle battle){
         // loop through all the targets
         if (activechar.getSpecialCooldown() > 0){
-            int ABE = 0;
             for (Combatant enemy : battle.getAllEnemies()){
                 if (!enemy.isAlive()){
                     continue; // skip over enemies that are already dead
                 } else {
                     super.execute(activechar, enemy, battle);
                     if (!enemy.isAlive()) {
-                        ABE += 1;
+                        ArcaneBlastEffect abe = new ArcaneBlastEffect();
+                        activechar.onApplyEffect(abe);
                     } // each kill adds 10 to attack given by arcane blast bonus. 
                 }
             }
-            statusEffect buff = new ArcaneBlastEffect(ABE);
-            activechar.setStatusEffect(buff);
             activechar.setSpecialCooldown(); // set special cooldown
             return true; // I still need to figure out how the status effects work. 
         }
