58dab172df52d14ba416b46465222d277c479568
Jarrett Lee <jarrettleehong@gmail.com>
2026-04-11 14:14:25 +0800

    added UI integration for future UI implementation

--- src/action/BasicAttack.java
+++ src/action/BasicAttack.java
@@ -3,15 +3,19 @@ import statusEffects.*;
 import entities.*;
 
 public class BasicAttack implements Action{
+    private int dmg;
     @Override
     public <T> boolean execute(T activechar, T target, Battle battle){
         int atk = activechar.getAttack();
         int def = target.getDefense();
-        int dmg = Math.max(0,atk - def);
-        target.takeDamage(dmg);
+        this.dmg = Math.max(0,atk - def);
+        target.takeDamage(this.dmg);
     }
     @Override
     public string getName(){
         return "Basic Attack";
+    }
+    public int getDamage(){
+        return dmg;
     }
 }
