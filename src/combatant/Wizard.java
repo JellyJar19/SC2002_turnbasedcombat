2497354a3897d53d22dbcbdb3095cab5b26f8103
Jarrett Lee <jarrettleehong@gmail.com>
2026-04-14 16:01:21 +0800

    Removed unnecessary comments

--- src/combatant/Wizard.java
+++ src/combatant/Wizard.java
@@ -17,25 +17,6 @@ public class Wizard extends Combatant {
 
     @Override
     public void performTurn(BattleContext context) {
-        //to be implemented
-        // switch (playerinput){
-        //     case 1: {
-        //          select target 
-        //          this.executeAction(BasicAttack, target, context);
-        //     }
-        //     case 2: {
-        //          select target
-        //          this.executeAction(SpecialSkill, target, context);
-        //     }
-        //     case 3: {
-        //          this.executeAction(Defend, this, context);
-        //     }
-        // case 4: {
-        //          this.executeAction(UseItem, this, context);
-        //          if item is powerstone, select target and executeAction(SpecialSkill, target, context);
-        //     }
-        //     default: {null}
-        // }
-
+        // handled by UI 
     }
 }
