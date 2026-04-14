e4c85aeca8a29336fe36152d1a04942cd92ac04d
Jarrett Lee <jarrettleehong@gmail.com>
2026-04-14 16:11:40 +0800

    Removed unnecessary comments

--- src/action/UseItem.java
+++ src/action/UseItem.java
@@ -13,7 +13,6 @@ public class UseItem extends AbstractActions{
     @Override
     public <T extends Combatant> boolean execute(T activechar, T target, BattleContext battle){
         return item.use(activechar, battle);
-        // will need to relook at this w further discussion for the powerstone item - because taht requires specialskill. 
     }
     
     public String getName(){
